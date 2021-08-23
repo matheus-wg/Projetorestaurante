5143b9dbef7a977dcf02f9bcabe37fea20901b08
matheus winter <matteus_mi@hotmail.com>
2021-08-23 01:51:52 -0300

    first commit

---
+++ javascript/produtos.js
@@ -0,0 +1,74 @@
+const produtos = [
+    {
+      id: 1,
+      image: 'https://picsum.photos/200',
+      title: 'Produto 01',
+      value: 10.00,
+      description: 'Descrição do produto 01',
+    
+    },
+
+    {
+        id: 2,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 02',
+        value: 20.00,
+        description: 'Descrição do produto 02',
+      
+    },
+
+    {
+        id: 3,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 03',
+        value: 30.00,
+        description: 'Descrição do produto 03',
+      
+    },
+
+    {
+        id: 4,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 04',
+        value: 40.00,
+        description: 'Descrição do produto 04',
+      
+    },
+
+    {
+        id: 5,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 05',
+        value: 50.00,
+        description: 'Descrição do produto 05',
+      
+    },
+
+    {
+        id: 6,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 06',
+        value: 60.00,
+        description: 'Descrição do produto 06',
+      
+    },
+      
+    {
+        id: 7,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 07',
+        value: 70.00,
+        description: 'Descrição do produto 07',
+      
+    },    
+      
+    {
+        id: 8,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 08',
+        value: 80.00,
+        description: 'Descrição do produto 08',
+      
+    },
+
+]
